48ca11762b0f359d3ca6330237f06c52135083a1
marc-hendrik-sieverding <155987457+marc-hendrik-sieverding@users.noreply.github.com>
2024-02-11 14:00:55 +0000

    Erstes Textfeld

--- zeit.js
+++ zeit.js
@@ -1,1 +1,23 @@
-test
+import React, { useState } from 'react';
+
+function TextField() {
+
+  const [textValue, setTextValue] = useState('');
+
+  const Eingabe = (event) => {
+    setTextValue(event.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={textValue}
+        onChange={Eingabe}
+        placeholder="Benutzername"
+      />
+      <p>Du hast eingegeben: {textValue}</p>
+    </div>
+  );
+}
+export default TextField;
