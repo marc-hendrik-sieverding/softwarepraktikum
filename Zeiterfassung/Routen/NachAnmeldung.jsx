e5a421db69af7a871d99da655863123c43793d1f
marc-hendrik-sieverding <155987457+marc-hendrik-sieverding@users.noreply.github.com>
2024-02-12 19:45:49 +0000

    Routen + Textfelder

---
+++ Zeiterfassung/Routen/NachAnmeldung.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NachAnmeldung() {
+    return <h2>Angemeldet</h2>;
+}
--- Zeiterfassung/src/App.jsx
+++ Zeiterfassung/src/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import './App.css'
+import { TextField } from '@react-ui-org/react-ui';
+import React from 'react';
+import './App.css';
+import NachAnmeldung from '../Routen/NachAnmeldung';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-     <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue="Username"
-        />
+    <div className="wrapper">
+      <h1>Login</h1>
+    </div>
+    <BrowserRouter>
+        <Switch>
+          <Route path="/home">
+            <NachAnmeldung />
+          </Route>
+        </Switch>
+    </BrowserRouter>
+     <TextField  placeholder = "Benutzername" onChange={() => {}}  required/>
+     <TextField minLength={8} placeholder = "Passwort" variant= "filled" required/>
     </>
   )
 }
