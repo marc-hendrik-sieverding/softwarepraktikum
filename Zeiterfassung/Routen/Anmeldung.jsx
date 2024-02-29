02a25ec768b4bfa2556d201919273b1dcc64a2f8
Marc Sieverding <mh.sieverding@stud.phwt.de>
2024-02-29 17:39:39 +0100

    umschreiben anmeldelogik

--- Zeiterfassung/Routen/Anmeldung.jsx
+++ Zeiterfassung/Routen/Anmeldung.jsx
@@ -1,27 +1,24 @@
 import { TextField} from '@react-ui-org/react-ui';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Button from '@mui/material/Button';
-import Registrierung from './Registrierung';
+import { Link } from 'react-router-dom';
 
-function Anmeldung() {
+function Anmeldung({ registrierteUser }) {
 
     const [Benutzername, setBenutzername] = useState('');
     const [Passwort, setPasswort] = useState('');
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [registrierteUser, setRegistrierteUser] = useState([]);
-
-    const handleRegistrierung = (Benutzername, Passwort) => {
-      const neuerBenutzer = { Benutzername: Benutzername, Passwort: Passwort };
-      setRegistrierteUser([...registrierteUser, neuerBenutzer]);
-  };
+    
 
     const Einloggen = () => {
-      if (Benutzername === 'Marc' && Passwort === 'Passwort1234') {
-        setIsLoggedIn(true);} 
-          else {
+      const user = registrierteUser.find(user => user.Benutzername === Benutzername && user.Passwort === Passwort);
+        if (user) {
+            setIsLoggedIn(true);
+        }
         alert('Falscher Benutzername oder Passwort');
       }
-  };
+
+
     const Ausloggen = () => {
       setIsLoggedIn(false);
     }
@@ -32,8 +29,8 @@ function Anmeldung() {
       {!isLoggedIn && (
           <div className="wrapper">
             <h1>Login</h1>
-              <TextField class="Benutzername" placeholder="Benutzername" onChange={(event) => setBenutzername(event.target.value)} required size="normal"/>
-              <TextField class="Passwort" placeholder="Passwort" variant="filled" onChange={(event) => setPasswort(event.target.value)} required minLength={8} type="password" size="normal"/>
+              <TextField class="Benutzername" placeholder="Benutzername" onChange={(event) => setBenutzername(event.target.value)} size="normal"/>
+              <TextField class="Passwort" placeholder="Passwort" variant="filled" onChange={(event) => setPasswort(event.target.value)} minLength={8} type="password" size="normal"/>
               <Button class = "Login" variant="contained" onClick={Einloggen}> Login </Button>
               <Link to ="/Registrierung">
               <Button class = "Login" variant="contained"> Registrierung </Button> </Link>
--- Zeiterfassung/Routen/Registrierung.jsx
+++ Zeiterfassung/Routen/Registrierung.jsx
@@ -1,19 +1,33 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { TextField } from '@react-ui-org/react-ui';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 
-function Registrierung({ handleRegistrierung }) {
+function Registrierung() {
     const [Benutzername, setBenutzername] = useState('');
     const [Passwort, setPasswort] = useState('');
+    const [registrierteUser, setRegistrierteUser] = useState([]);
+    const [error, setError] = useState(false);
+
+    const handleRegistrierung = (Benutzername, Passwort) => {
+        const neuerBenutzer = { Benutzername: Benutzername, Passwort: Passwort };
+        setRegistrierteUser([...registrierteUser, neuerBenutzer]);
+    };
 
     const handleRegister = () => {
-        handleRegistrierung(Benutzername, Passwort);
+        if(Benutzername.trim() === '' || Passwort.trim() === '') {
+            setError(true)
+        }
+       else { handleRegistrierung(Benutzername, Passwort);
         setBenutzername('');
-        setPasswort('');
+        setPasswort(''); }
     };
+    console.log(error)
+    console.log(Benutzername, Passwort)
+
     return (
         <>
+        {error && <h1>Bitte fülle alle Felder aus</h1>}
             <h2>Registrierung</h2>
             <TextField placeholder="Benutzername" value={Benutzername} onChange={(event) => setBenutzername(event.target.value)} />
             <TextField placeholder="Passwort" variant="filled" value={Passwort} onChange={(event) => setPasswort(event.target.value)} type="password"/>
